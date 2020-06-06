d8daaa857b1aeedf72442c1183a8d4040cbe8476
john001 <johnnyhe2015@gmail.com>
2020-06-06 12:19:44 -0400

    refactor structure

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID:'962101925725-1m7rjdqvo19jk4b5vopem9vum5l6p3tb.apps.googleusercontent.com',
+    googleClientSecret:'N7ToU9yhp68PqiiKd7esE619'
+}
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+authRoutes(app);
+
 app.get('/',(req,res)=>{
-    res.send({hi:'there1'})
+    res.send({hi:'there2'})
 });
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
